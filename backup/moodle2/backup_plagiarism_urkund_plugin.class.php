3e60100504a5fab85d733a8aff02a6a567d60b96
Dan Marsden <dan@danmarsden.com>
2011-08-01 10:23:58 +1200

    URKUND backup/restore code

---
+++ backup/moodle2/backup_plagiarism_urkund_plugin.class.php
@@ -0,0 +1,56 @@
+<?php
+
+defined('MOODLE_INTERNAL') || die();
+
+
+class backup_plagiarism_urkund_plugin extends backup_plagiarism_plugin {
+    function define_module_plugin_structure() {
+        // To know if we are including userinfo
+        $userinfo = $this->get_setting_value('userinfo');
+
+        // Define the virtual plugin element without conditions as the global class checks already.
+        $plugin = $this->get_plugin_element();
+
+        // Create one standard named plugin element (the visible container)
+        $pluginwrapper = new backup_nested_element($this->get_recommended_name());
+
+        // connect the visible container ASAP
+        $plugin->add_child($pluginwrapper);
+
+        $urkundconfigs = new backup_nested_element('urkund_configs');
+        $urkundconfig = new backup_nested_element('urkund_config', array('id'), array('name', 'value'));
+        $pluginwrapper->add_child($urkundconfigs);
+        $urkundconfigs->add_child($urkundconfig);
+        $urkundconfig->set_source_table('urkund_config', array('cm' => backup::VAR_PARENTID));
+
+        //now information about files to module
+        $urkundfiles = new backup_nested_element('urkund_files');
+        $urkundfile = new backup_nested_element('urkund_file', array('id'),
+                            array('userid', 'identifier','filename','reporturl','optout','statuscode','similarityscore','errorresponse','timesubmitted'));
+
+        $pluginwrapper->add_child($urkundfiles);
+        $urkundfiles->add_child($urkundfile);
+        if ($userinfo) {
+            $urkundfile->set_source_table('urkund_files', array('cm' => backup::VAR_PARENTID));
+        }
+        return $plugin;
+    }
+
+    function define_course_plugin_structure() {
+        // Define the virtual plugin element without conditions as the global class checks already.
+        $plugin = $this->get_plugin_element();
+
+        // Create one standard named plugin element (the visible container)
+        $pluginwrapper = new backup_nested_element($this->get_recommended_name());
+
+        // connect the visible container ASAP
+        $plugin->add_child($pluginwrapper);
+        //save id from urkund course
+        $urkundconfigs = new backup_nested_element('urkund_configs');
+        $urkundconfig = new backup_nested_element('urkund_config', array('id'), array('plugin', 'name', 'value'));
+        $pluginwrapper->add_child($urkundconfigs);
+        $urkundconfigs->add_child($urkundconfig);
+        $urkundconfig->set_source_table('config_plugins', array('name'=> backup::VAR_PARENTID, 'plugin' => backup_helper::is_sqlparam('plagiarism_urkund_course')));
+        return $plugin;
+    }
+}
---
+++ backup/moodle2/restore_plagiarism_urkund_plugin.class.php
@@ -0,0 +1,73 @@
+<?php
+
+defined('MOODLE_INTERNAL') || die();
+
+
+class restore_plagiarism_urkund_plugin extends restore_plagiarism_plugin {
+    /**
+     * Returns the paths to be handled by the plugin at question level
+     */
+    protected function define_course_plugin_structure() {
+        $paths = array();
+
+        // Add own format stuff
+        $elename = 'urkundconfig';
+        $elepath = $this->get_pathfor('urkund_configs/urkund_config'); // we used get_recommended_name() so this works
+        $paths[] = new restore_path_element($elename, $elepath);
+
+        return $paths; // And we return the interesting paths
+    }
+
+    public function process_urkundconfig($data) {
+        $data = (object)$data;
+
+        if ($this->task->is_samesite()) { //files can only be restored if this is the same site as was backed up.
+            //only restore if a link to this course doesn't already exist in this install.
+            set_config($this->task->get_courseid(), $data->value, $data->plugin);
+        }
+    }
+
+    /**
+     * Returns the paths to be handled by the plugin at module level
+     */
+    protected function define_module_plugin_structure() {
+        $paths = array();
+
+        // Add own format stuff
+        $elename = 'urkundconfigmod';
+        $elepath = $this->get_pathfor('urkund_configs/urkund_config'); // we used get_recommended_name() so this works
+        $paths[] = new restore_path_element($elename, $elepath);
+
+        $elename = 'urkundfiles';
+        $elepath = $this->get_pathfor('/urkund_files/urkund_file'); // we used get_recommended_name() so this works
+        $paths[] = new restore_path_element($elename, $elepath);
+
+        return $paths; // And we return the interesting paths
+
+    }
+
+    public function process_urkundconfigmod($data) {
+        global $DB;
+
+        if ($this->task->is_samesite()) { //files can only be restored if this is the same site as was backed up.
+            $data = (object)$data;
+            $oldid = $data->id;
+            $data->cm = $this->task->get_moduleid();
+
+            $DB->insert_record('urkund_config', $data);
+        }
+    }
+
+    public function process_urkundfiles($data) {
+        global $DB;
+
+        if ($this->task->is_samesite()) { //files can only be restored if this is the same site as was backed up.
+            $data = (object)$data;
+            $oldid = $data->id;
+            $data->cm = $this->task->get_moduleid();
+            $data->userid = $this->get_mappingid('user', $data->userid);
+
+            $DB->insert_record('urkund_files', $data);
+        }
+    }
+}
