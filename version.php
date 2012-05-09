f50830adad0a608d7fbc6331f117e010a9c7ffe5
Dan Marsden <dan@danmarsden.com>
2012-05-09 14:23:50 +1200

    bump version as file handling has changed in preparation for Moodle 2.3

--- version.php
+++ version.php
@@ -14,9 +14,9 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-$plugin->version =  2011121201;
-$plugin->requires = 2011120501.06;
+$plugin->version =  2012050900;
+$plugin->requires = 2012051800.00;
 $plugin->cron     = 300; //only run every 5 minutes
 $plugin->component = 'plagiarism_urkund';
-$plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '1.2';
+$plugin->maturity  = MATURITY_ALPHA;
+$plugin->release   = '1.3';
