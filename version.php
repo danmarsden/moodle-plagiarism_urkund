d8360d394d5382b0a27386bf69e615867ce3b048
Dan Marsden <dan@danmarsden.com>
2013-02-08 23:02:02 +1300

    bump version

--- version.php
+++ version.php
@@ -14,9 +14,9 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-$plugin->version =  2012050901;
+$plugin->version =  2012050902;
 $plugin->requires = 2012062500.00;
 $plugin->cron     = 300; //only run every 5 minutes
 $plugin->component = 'plagiarism_urkund';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '1.4.1';
+$plugin->release   = '1.4.2';
