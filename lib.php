072cfd1c62b1c5b6bcaaf58b5702e47d8c148a84
Dan Marsden <dan@danmarsden.com>
2013-05-23 12:39:57 +1200

    fix settings for 2.5

--- lib.php
+++ lib.php
@@ -686,10 +686,11 @@ function urkund_get_form_elements($mform) {
             PLAGIARISM_URKUND_DRAFTSUBMIT_FINAL => get_string("submitonfinal", "plagiarism_urkund")
             );
 
-    $mform->addElement('header', 'plagiarismdesc');
+    $mform->addElement('header', 'plagiarismdesc', get_string('urkund', 'plagiarism_urkund'));
     $mform->addElement('select', 'use_urkund', get_string("useurkund", "plagiarism_urkund"), $ynoptions);
     $mform->addElement('text', 'urkund_receiver', get_string("urkund_receiver", "plagiarism_urkund"), array('size'=>40));
     $mform->addHelpButton('urkund_receiver', 'urkund_receiver', 'plagiarism_urkund');
+    $mform->setType('urkund_receiver', PARAM_TEXT);
     $mform->addElement('select', 'urkund_show_student_score', get_string("urkund_show_student_score", "plagiarism_urkund"), $tiioptions);
     $mform->addHelpButton('urkund_show_student_score', 'urkund_show_student_score', 'plagiarism_urkund');
     $mform->addElement('select', 'urkund_show_student_report', get_string("urkund_show_student_report", "plagiarism_urkund"), $tiioptions);
