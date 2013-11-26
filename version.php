023da1c5a416915072a7c70927791ef4189271a9
Dan Marsden <dan@danmarsden.com>
2013-11-27 12:31:34 +1300

    bump version for new debugging/reports

--- version.php
+++ version.php
@@ -23,9 +23,9 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$plugin->version =  2012050905;
+$plugin->version =  2012050906;
 $plugin->requires = 2012110900.00;
 $plugin->cron     = 300; //only run every 5 minutes
 $plugin->component = 'plagiarism_urkund';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '1.5.4';
+$plugin->release   = '1.5.5';
