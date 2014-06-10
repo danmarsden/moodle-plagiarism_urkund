74f053c10d4c176688e85deb48652d4a39edd388
Dan Marsden <dan@danmarsden.com>
2014-06-11 11:47:49 +1200

    Bump version with new ajax validation on receiver address and other fixes

--- version.php
+++ version.php
@@ -23,9 +23,9 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$plugin->version = 2013112700;
+$plugin->version = 2014061100;
 $plugin->requires = 2013051400.00;
 $plugin->cron     = 300; // Only run every 5 minutes.
 $plugin->component = 'plagiarism_urkund';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '1.6.2';
+$plugin->release   = '1.7.1';
