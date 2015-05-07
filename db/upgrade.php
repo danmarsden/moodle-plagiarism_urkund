112012255d2fc5dd3a13b26d1a44f834e853af21
Dan Marsden <dan@danmarsden.com>
2015-05-08 09:49:12 +1200

    Fix default API address and update incorrect/old addresses

--- db/upgrade.php
+++ db/upgrade.php
@@ -48,5 +48,16 @@ function xmldb_plagiarism_urkund_upgrade($oldversion) {
         upgrade_plugin_savepoint(true, 2013081900, 'plagiarism', 'urkund');
     }
 
+    if ($oldversion < 2013112706) {
+        // Check for old API address and update if required.
+        $apiaddress = get_config('plagiarism', 'urkund_api');
+        if ($apiaddress == 'https://secure.urkund.com/ws/integration/1.0/rest/submissions' ||
+            $apiaddress == 'https://secure.urkund.com/api/rest/submissions' ) {
+            set_config('urkund_api', 'https://secure.urkund.com/api', 'plagiarism');
+        }
+
+        upgrade_plugin_savepoint(true, 2013112706, 'plagiarism', 'urkund');
+    }
+
     return true;
 }
