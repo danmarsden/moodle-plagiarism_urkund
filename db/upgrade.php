45f5f52e6efba21635dcc60f92a32607d1821c1b
Dan Marsden <dan@danmarsden.com>
2015-05-21 09:33:17 +1200

    Further corrections to default API location as requested by URKUND

--- db/upgrade.php
+++ db/upgrade.php
@@ -54,15 +54,17 @@ function xmldb_plagiarism_urkund_upgrade($oldversion) {
         upgrade_plugin_savepoint(true, 2012050904, 'plagiarism', 'urkund');
     }
 
-    if ($oldversion < 2012050907) {
+    if ($oldversion < 2012050908) {
         // Check for old API address and update if required.
         $apiaddress = get_config('plagiarism', 'urkund_api');
         if ($apiaddress == 'https://secure.urkund.com/ws/integration/1.0/rest/submissions' ||
-            $apiaddress == 'https://secure.urkund.com/api/rest/submissions' ) {
-            set_config('urkund_api', 'https://secure.urkund.com/api', 'plagiarism');
+            $apiaddress == 'https://secure.urkund.com/api/rest/submissions' ||
+            $apiaddress == 'https://secure.urkund.com/api') {
+
+            set_config('urkund_api', 'https://secure.urkund.com/api/submissions', 'plagiarism');
         }
 
-        upgrade_plugin_savepoint(true, 2012050907, 'plagiarism', 'urkund');
+        upgrade_plugin_savepoint(true, 2012050908, 'plagiarism', 'urkund');
     }
 
     return true;
