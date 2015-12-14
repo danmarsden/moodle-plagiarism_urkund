1580fd5bbcef2397fb21a038bfcc0fa00e3e5e76
Dan Marsden <dan@danmarsden.com>
2015-12-14 21:48:55 +1300

    Bump version for plugins db release

--- version.php
+++ version.php
@@ -23,9 +23,9 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$plugin->version = 2015120801;
+$plugin->version = 2015121400;
 $plugin->requires = 2014041100.00;
 $plugin->cron     = 86400; // Cron function no longer used.
 $plugin->component = 'plagiarism_urkund';
-$plugin->maturity  = MATURITY_BETA;
-$plugin->release   = '3.2.0';
+$plugin->maturity  = MATURITY_STABLE;
+$plugin->release   = '3.2.1';
