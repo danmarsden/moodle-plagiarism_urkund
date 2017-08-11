80914a7b5d75a508a139ab8d7c72643b8a0a0f5a
Jordi Pujol-Ahulló <jpahullo@gmail.com>
2017-08-11 02:15:41 +0200

    Allow configuration to define which options are advanced settings also add ability to hide advanced settings from teachers.

--- urkund_form.php
+++ urkund_form.php
@@ -89,7 +89,7 @@ class urkund_defaults_form extends moodleform {
     // Define the form.
     public function definition () {
         $mform =& $this->_form;
-        urkund_get_form_elements($mform);
+        urkund_get_form_elements($mform, true);
         $this->add_action_buttons(true);
     }
 }
