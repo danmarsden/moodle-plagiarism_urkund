abf88029c1fd5afe70c44907f65c89a4e2f3ccc9
Dan Marsden <dan@danmarsden.com>
2017-08-22 12:56:43 +1200

    bump version for plugins db release.

--- version.php
+++ version.php
@@ -25,9 +25,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2017020803;
+$plugin->version = 2017020804;
 $plugin->requires = 2016050600.00;
 $plugin->cron     = 0; // Cron function no longer used.
 $plugin->component = 'plagiarism_urkund';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '3.2.9';
+$plugin->release   = '3.2.10';
