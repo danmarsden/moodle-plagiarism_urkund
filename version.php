34a6f13cf09e249b2b14f6c03dad4f22a8824921
Dan Marsden <dan@danmarsden.com>
2018-01-17 10:20:07 +1300

    Use a different identifier when resubmitting to ensure an updated report is provided.

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2017020807;
+$plugin->version = 2017020808;
 $plugin->requires = 2016050600.00;
 $plugin->cron     = 0; // Cron function no longer used.
 $plugin->component = 'plagiarism_urkund';
