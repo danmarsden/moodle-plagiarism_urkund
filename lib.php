9ae5524349ff9361ed09d6fa9ceffc19e5f59c5a
Dan Marsden <dan@danmarsden.com>
2018-06-20 09:31:09 +1200

    fix some coding guideline things.

--- lib.php
+++ lib.php
@@ -899,7 +899,8 @@ function urkund_get_form_elements($mform) {
         $resubmitoptions = array(PLAGIARISM_URKUND_RESUBMITNO => get_string('no'),
             PLAGIARISM_URKUND_RESUBMITDUEDATE => get_string('resubmitdue', 'plagiarism_urkund'),
             PLAGIARISM_URKUND_RESUBMITCLOSEDATE => get_string('resubmitclose', 'plagiarism_urkund'));
-        $mform->addElement('select', 'urkund_resubmit_on_close', get_string("urkund_resubmit_on_close", "plagiarism_urkund"), $resubmitoptions);
+        $mform->addElement('select', 'urkund_resubmit_on_close', get_string("urkund_resubmit_on_close", "plagiarism_urkund"),
+            $resubmitoptions);
         $mform->addHelpButton('urkund_resubmit_on_close', 'urkund_resubmit_on_close', 'plagiarism_urkund');
         $mform->setType('urkund_resubmit_on_close', PARAM_INT);
     }
@@ -1938,7 +1939,8 @@ function plagiarism_urkund_resubmit_on_close() {
               JOIN {course_modules} cm ON cm.instance = a.id
               JOIN {modules} m ON m.id = cm.module
               JOIN {plagiarism_urkund_config} uc ON uc.cm = cm.id AND uc.name = 'use_urkund' AND uc.value = '1'
-              JOIN {plagiarism_urkund_config} uc1 ON uc1.cm = cm.id AND uc1.name = 'urkund_resubmit_on_close' AND uc1.value = :resubmit
+              JOIN {plagiarism_urkund_config} uc1 ON uc1.cm = cm.id AND uc1.name = 'urkund_resubmit_on_close'
+                                                  AND uc1.value = :resubmit
          LEFT JOIN {plagiarism_urkund_config} uc2 ON uc2.cm = cm.id AND uc2.name = 'timeresubmitted'
              WHERE m.name = 'assign' AND a.duedate > 1 AND a.duedate < :now
                    AND uc2.value IS NULL OR ". $DB->sql_cast_char2int('uc2.value'). " < a.duedate";
@@ -1967,7 +1969,11 @@ function plagiarism_urkund_resubmit_on_close() {
     }
 }
 
-
+/**
+ * Function used to trigger resubmission for all files in a cm.
+ *
+ * @param int $cmid
+ */
 function plagiarism_urkund_resubmit_cm($cmid) {
     global $DB;
 
