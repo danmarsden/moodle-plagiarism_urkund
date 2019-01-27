7fbb0cff066567362963712fb238177ca0c671c2
Dan Marsden <dan@danmarsden.com>
2019-01-28 10:32:22 +1300

    Bump version for new capability.

--- version.php
+++ version.php
@@ -25,9 +25,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2018102300;
+$plugin->version = 2019012800;
 $plugin->requires = 2017111300; // Requires 3.4.
 $plugin->cron     = 0; // Cron function no longer used.
 $plugin->component = 'plagiarism_urkund';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '3.3.8';
+$plugin->release   = '3.3.9';
