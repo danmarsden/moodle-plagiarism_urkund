5d6835a71366069e846f80d413e707defec6efac
Dan Marsden <dan@danmarsden.com>
2020-05-13 14:46:47 +1200

    Bump plugin version.

--- version.php
+++ version.php
@@ -25,9 +25,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2020051101;
+$plugin->version = 2020051300;
 $plugin->requires = 2020030600; // Requires 3.9.
 $plugin->cron     = 0; // Cron function no longer used.
 $plugin->component = 'plagiarism_urkund';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '3.9.2';
+$plugin->release   = '3.9.3';
