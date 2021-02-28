d7d408d532da9a16c4b537f618a804f90b79233c
Dan Marsden <dan@danmarsden.com>
2021-03-01 10:38:32 +1300

    Fix some other references to Urkund.

--- lang/en/plagiarism_urkund.php
+++ lang/en/plagiarism_urkund.php
@@ -156,7 +156,7 @@ $string['urkunddefaults_forum'] = 'Default forum settings';
 $string['urkunddefaults_workshop'] = 'Default workshop settings';
 $string['urkunddefaults_hsuforum'] = 'Default hsuforum settings';
 $string['urkunddefaults_quiz'] = 'Default quiz settings';
-$string['urkundexplain'] = 'For more information on this plugin see: <a href="http://www.urkund.com/en" target="_blank">http://www.urkund.com/int/en/</a>';
+$string['urkundexplain'] = 'For more information on this plugin see: <a href="https://ouriginal.com" target="_blank">ouriginal.com</a>';
 $string['urkundfiles'] = 'Ouriginal Files';
 $string['useurkund'] = 'Enable Ouriginal';
 $string['waitingevents'] = 'There are {$a->countallevents} events waiting for cron and {$a->countheld} events are being held for resubmission';
@@ -165,7 +165,7 @@ $string['charcount_help'] = 'This sets a minimum limit on the number of characte
 $string['cannotupgradeunprocesseddata'] = '';
 $string['cannotupgradeunprocesseddata'] = '<h1>Cannot upgrade to this version of the plugin due to existing unprocessed data, please revert to an earlier version of this plugin and clear old events.</h1><p>This version of the plugin relies on the new events API in Moodle but your installation contains un-processed events related to the old API.</p>
  You should revert to an older version of the Ouriginal plugin, put the site into maintenance mode, run the Moodle Cron process and make sure all old events are cleared. Then try upgrading to this version of the Ouriginal plugin again.</p>
- <p>For more information see: <a href="https://docs.moodle.org/en/Plagiarism_Prevention_URKUND_Settings#Installation_failed_due_to_unprocessed_data">Ouriginal Installation failed due to unprocessed data</a></p>';
+ <p>For more information see: <a href="https://docs.moodle.org/en/Plagiarism_Prevention_Ouriginal_Settings#Installation_failed_due_to_unprocessed_data">Ouriginal Installation failed due to unprocessed data</a></p>';
 $string['timesubmitted'] = 'Time submitted';
 $string['urkund:advancedsettings'] = 'Allow the teacher to view advanced module settings from Ouriginal';
 $string['resubmitdue'] = 'Resubmit after due date';
