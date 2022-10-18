b1921696fe847e965672239a486c80c57c20f041
Dan Marsden <dan@danmarsden.com>
2022-10-19 08:53:13 +1300

    bump version
    bump version.

--- version.php
+++ version.php
@@ -25,9 +25,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2021081801;
+$plugin->version = 2021081802;
 $plugin->requires = 2020030600; // Requires 3.9.
 $plugin->cron     = 0; // Cron function no longer used.
 $plugin->component = 'plagiarism_urkund';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '3.9.18';
+$plugin->release   = '3.9.19';
